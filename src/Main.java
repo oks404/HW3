aae8d220dd9a45cd928b1f4c044a71e98495ae19
oks404 <ksunya2004@list.ru>
2022-11-10 19:28:08 +1000

    Задания 1, 2, 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-
-
+//  Задание 1
+        int apple = 1;
+        byte melon = 2;
+        short banana = 3;
+        float watermelon = 4;
+        double pear = 5;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        System.out.println("Значение переменной melon с типом byte равно " + melon);
+        System.out.println("Значение переменной banana с типом short равно " + banana);
+        System.out.println("Значение переменной watermelon с типом float равно " + watermelon);
+        System.out.println("Значение переменной pear с типом double равно " + pear);
+//  Задание 2
+        float dog = 27.12f;
+        long cat = 987678965549L;
+        double elefant = 2.786;
+        boolean snake = false;
+        short horse = 569;
+        short duck = -159;
+        short goose = 27897;
+        byte chicken = 67;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(elefant);
+        System.out.println(snake);
+        System.out.println(horse);
+        System.out.println(duck);
+        System.out.println(goose);
+        System.out.println(chicken);
+//  Задание 3
+        byte pupilsLudmilaPavlovna = 23;
+        byte pupilsAnnaSergeevna = 27;
+        byte pupilsEkaterinaAndreevna = 30;
+        short paper = 480;
+        System.out.println("На каждого ученика рассчитано " + paper / (pupilsLudmilaPavlovna + pupilsAnnaSergeevna + pupilsEkaterinaAndreevna) + " листов бумаги");
     }
 }
